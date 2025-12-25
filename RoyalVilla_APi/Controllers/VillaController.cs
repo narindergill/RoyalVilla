4eee1eef33138e5265ec7832ec258a2e8f27a8ba
Narinder Singh Gill <nsgalib@outlook.com>
2025-12-25 11:30:05 +0000

    Add project files.

---
+++ RoyalVilla_APi/Controllers/VillaController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RoyalVilla_APi.Controllers
+{
+    [Route("api/villa")]
+    [ApiController]
+    public class VillaController : ControllerBase
+    {
+        [HttpGet]
+        public string GetVillas()
+        {
+            return "get all villas";
+        }
+
+        [HttpGet("{id:int}")]
+        public string GetVillaById(int id)
+        {
+            return $"get villa with id: {id}";
+        }
+    }
+}
---
+++ RoyalVilla_APi/Program.cs
@@ -0,0 +1,43 @@
+using Microsoft.EntityFrameworkCore;
+using RoyalVilla_APi.Data;
+using Scalar.AspNetCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+await SeedDataAsync(app);
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.MapScalarApiReference();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+
+
+// Seed initial data and any pending migrations
+static async Task SeedDataAsync(WebApplication app)
+{
+    using var scope = app.Services.CreateScope();
+    var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+
+    await context.Database.MigrateAsync();
+}
